d9e1fad75f4340fcf0bfdcdecc39d0b114316e33
LDF <liudengfeng_sd@outlook.com>
2023-03-19 06:05:49 +0800

    codon

---
+++ demo_mcts.py
@@ -0,0 +1,20 @@
+from muzero.config import MuZeroConfig
+from muzero.apply import initial_inference, get_muzero_action
+import xqcpp
+import time
+
+config = MuZeroConfig()
+config.runs = 5
+config.init_fen = "2b6/3ka4/2Pa5/3N5/9/3R5/9/9/5pr2/3AK4 r - 110 0 190"
+config.restore_from_latest_checkpoint = True
+print(initial_inference(config))
+
+start = time.time()
+config.num_simulations = 240
+moves = ["2737", "3839", "3738", "3949"]
+action = get_muzero_action(config, moves)
+print(
+    "Mcts tree search move = {} duration = {:.2f}s".format(
+        xqcpp.a2m(action), time.time() - start
+    )
+)
--- muzero/apply.py
+++ muzero/apply.py
@@ -1,15 +1,37 @@
 import gymnasium as gym
 import gymxq
+import numpy as np
 import torch
 import xqcpp
-from .feature_utils import obs2feature, encoded_action
-from .models import MuZeroNetwork
 from gymxq.constants import NUM_ACTIONS
-import numpy as np
+
+from .checkpoint import get_current_checkpoint
+from .feature_utils import encoded_action, obs2feature
+from .mcts import MCTS, render_root
+from .models import MuZeroNetwork
+from .self_play import select_action
 
 
-def predicate(config):
+def get_pretrained_model(config):
+    """训练模型
+
+    Args:
+        config (配置): 配置对象
+
+    Returns:
+        model: 模型
+    """
     model = MuZeroNetwork(config)
+    checkpoint = get_current_checkpoint(config)
+    model.set_weights(checkpoint["weights"])
+    print("model version = {:>5d}".format(checkpoint["model_version"]))
+    model = torch.compile(model)
+    model.eval()
+    return model
+
+
+def initial_inference(config):
+    model = get_pretrained_model(config)
     environment = gym.make("xqv1", init_fen=config.init_fen)
     obs, infos = environment.reset()
     observation = obs2feature(obs, flatten=False)
@@ -18,7 +40,6 @@ def predicate(config):
     #     [encoded_action(last_a)[np.newaxis, :], observation], axis=1
     # )
     observation = torch.tensor(observation).to(next(model.parameters()).device)
-    model.eval()
     with torch.no_grad():
         (
             root_predicted_value,
@@ -30,10 +51,51 @@ def predicate(config):
         torch.tensor([policy_logits[0][a] for a in infos["legal_actions"]]), dim=0
     ).tolist()
     policy = {
-        xqcpp.m2a(a): policy_values[i]
+        xqcpp.a2m(a): round(policy_values[i], 2)
         for i, a in enumerate(infos["legal_actions"])
     }
     return {
         "policy": policy,
-        "value": root_predicted_value.detach().cpu().numpy().item(),
+        "value": round(root_predicted_value.detach().cpu().numpy().item(), 2),
     }
+
+
+def get_muzero_action(config, moves=[], debug=True):
+    config.debug_mcts = True
+    model = get_pretrained_model(config)
+    environment = gym.make("xqv1", init_fen=config.init_fen)
+    obs, infos = environment.reset()
+    for m in moves:
+        _, _, _, _, infos = environment.step(xqcpp.m2a(m))
+    legal_actions = infos["legal_actions"]
+    legal_moves = []
+    for a in legal_actions:
+        legal_moves.append(xqcpp.a2m(a))
+    print("合法移动", legal_moves)
+    to_play = infos["to_play"]
+    observation = obs2feature(obs, flatten=False)
+    root, mcts_info = MCTS(config).run(
+        model,
+        observation,
+        legal_actions,
+        to_play,
+        False,
+    )
+    action = select_action(root, 0)
+    if debug:
+        filename = "episode_{:06d}_{:03d}".format(1, 1)
+        render_root(root, filename, "svg", "demo_mcts_tree")
+        sum_visits = sum(child.visit_count for child in root.children.values())
+        tips = [
+            (
+                xqcpp.a2m(a),
+                round(root.children[a].visit_count / sum_visits, 2),
+                # 应取反 【对手状态值】
+                round(-root.children[a].value(), 2),
+                round(root.children[a].prior, 2),
+            )
+            for a in legal_actions
+        ]
+        tips = sorted(tips, key=lambda x: x[1], reverse=True)
+        print(tips)
+    return action
